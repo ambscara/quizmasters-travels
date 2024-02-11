df4c582be7d71151cbce08c229692dac6969fe24
ambscara <150319615+ambscara@users.noreply.github.com>
2024-02-10 20:53:32 -0800

    started on JS

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+const questions = [
+    {
+        question: "What is the capital of France?",
+        options: ["Paris", "Berlin", "London", "Madrid"],
+        answer: "Paris"
+    },
+    {
+        question: "In what year did the great fire of London take place?",
+        options: ["1962", "1956", "1977", "1966"],
+        answer: "1966"
+    },
+]
+const questionElement = document.getElementById("question");
+const answerButton = document.getElementById("answer-buttons");
